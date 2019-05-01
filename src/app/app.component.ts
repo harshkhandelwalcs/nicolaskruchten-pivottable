ae1298679beeb5e26829cd7c3c20395bf2565fbd
harshkhandelwalcs <hkcs1995@gmail.com>
2019-05-01 15:01:28 +0530

    Add dynamic functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,87 @@ declare var $: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pivotTaBLE';
+  title = 'Pivot Table Demo';
+  public csvData: any;
   constructor() {
 
   }
-  ngOnInit() {
-    $("#output").pivotUI(
-      [
-        { color: "blue", shape: "circle" },
-        { color: "red", shape: "triangle" }
-      ],
-      {
-        rows: ["color"],
-        cols: ["shape"]
+  handleFileSelect(evt) {
+    const files = evt.target.files;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      const csv = reader.result;
+      this.csvData = this.csvToJson(csv);
+      console.log('csv', this.csvData);
+      let rowArray = [];
+      for (var key in this.csvData[0]) {
+        if (this.csvData[0].hasOwnProperty(key)) {
+          rowArray.push(key);
+        }
       }
-    );
+      if (this.csvData) {
+        $("#output").pivotUI(
+          this.csvData,
+          {
+            vals: rowArray
+          }
+        );
+
+      }
+    };
+
+  }
+  csvToJson(csv) {
+    const lines = csv.split('\n');
+    const result = [];
+    const headers = lines[0].split(',');
+
+    for (let i = 1; i < lines.length; i++) {
+      const obj = {};
+      const row = lines[i];
+      let queryIdx = 0;
+      let startValueIdx = 0;
+      let idx = 0;
+      if (row.trim() === '') { continue; }
+      while (idx < row.length) {
+        /* if we meet a double quote we skip until the next one */
+        let c = row[idx];
+        if (c === '"') {
+          do { c = row[++idx]; } while (c !== '"' && idx < row.length - 1);
+        }
+        if (c === ',' || /* handle end of line with no comma */ idx === row.length - 1) {
+          /* we've got a value */
+          let value = row.substr(startValueIdx, idx - startValueIdx).trim();
+          /* skip first double quote */
+          if (value[0] === '"') { value = value.substr(1); }
+          /* skip last comma */
+          if (value[value.length - 1] === ',') { value = value.substr(0, value.length - 1); }
+          /* skip last double quote */
+          if (value[value.length - 1] === '"') { value = value.substr(0, value.length - 1); }
+          const key = headers[queryIdx++];
+          obj[key] = value;
+          startValueIdx = idx + 1;
+        }
+        ++idx;
+      }
+      result.push(obj);
+    }
+    console.log(result);
+    return result;
+  }
+  ngOnInit() {
+    // $("#output").pivotUI(
+    //   [
+    //     { color: "blue", shape: "circle" },
+    //     { color: "red", shape: "triangle" }
+    //   ],
+    //   {
+    //     rows: ["color"],
+    //     cols: ["shape"]
+    //   }
+    // );
+
   }
 }
